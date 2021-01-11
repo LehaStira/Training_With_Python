13ebede148e19ee47402a8c24359878c930dd30b
Alexey Kolesnik <alexeyykolesnik@gmail.com>
2021-01-11 13:52:39 +0300

    Adding my training program to understand how property works

---
+++ test_for_property.py
@@ -0,0 +1,31 @@
+class MyClass:
+    @property
+    def real_a(self):
+        return self._real_a
+
+    @property
+    def real_a(self):
+        return self._real_a
+
+    def __init__(self, a):
+        self._a = a
+
+    real_a = property()
+
+    @real_a.getter
+    def real_a(self):
+        return self._a
+
+    @real_a.setter
+    def real_a(self, value):
+        if value > 0:
+            self._a = value
+        else:
+            raise ValueError('Значение а меньше нуля!')
+
+    def __str__(self):
+        return f'Значение а в данном объекте равно = {self.real_a}'
+
+if __name__ == "__main__":
+    my_c = MyClass(5)
+    print(my_c)
