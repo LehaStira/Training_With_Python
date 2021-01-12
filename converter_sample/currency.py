2068072abafc1313c6aa8629c4e65ac2a084f7a4
Alexey Kolesnik <alexeyykolesnik@gmail.com>
2021-01-12 22:32:20 +0300

    A lot of bugs to fix, but it must works...

--- converter_sample/currency.py
+++ converter_sample/currency.py
@@ -3,7 +3,39 @@ from decimal import Decimal
 import requests
 
 
-def convert(amount, cur_from, cur_to, requests, date = None):
+def get_soup(xml):
+    soup = BeautifulSoup(xml, 'lxml')
+    return soup
+
+
+def get_hyper_text_valute(soup, kod):
+    return soup.find('valute',
+                     id=kod)
+
+
+def get_nominal(soup, kod):
+    hyper_text = get_hyper_text_valute(soup, kod)
+    hyper_text_nominal = hyper_text.nominal
+    res = hyper_text_nominal.text
+    return res
+
+
+def get_value(soup, kod):
+    hyper_text = get_hyper_text_valute(soup, kod)
+    hyper_text_value = hyper_text.value
+    res = hyper_text_value.text
+    return res
+
+
+def get_course_of_exchange(value, nominal):
+    return value/nominal
+
+
+def _convert(amount, first_result, second_result):
+    return amount / first_result * second_result
+
+
+def convert(amount, cur_from, cur_to, date=None):
     """
     amount - сумма
     cur_from - код валюты, в которой передано значение
@@ -20,9 +52,26 @@ def convert(amount, cur_from, cur_to, requests, date = None):
     ulr = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=01/01/2021'
     response = requests.get(ulr)  # Использовать переданный requests
 
+    my_xml = response.content
 
+    soup = get_soup(my_xml)
 
-    result = Decimal('3754.8057')
+    first_nominal = get_nominal(soup, cur_from)
+    second_nominal = get_nominal(soup, cur_to)
+
+    first_value = get_value(soup, cur_from)
+    second_value = get_value(soup, cur_to)
+
+    first_result = get_course_of_exchange(nominal = first_nominal,
+                                          value = first_value)
+
+    second_result = get_course_of_exchange(nominal = second_nominal,
+                                           value = second_value)
+
+    res = _convert(amount, first_result, second_result)
+    res = str(res)
+
+    result = Decimal(res)
     return result  # не забыть про округление до 4х знаков после запятой
 
 
